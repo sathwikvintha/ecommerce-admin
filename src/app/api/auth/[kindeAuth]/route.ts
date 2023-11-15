616381896087743fd9ae6695b1f7336b177e1ef7
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2023-11-15 10:20:06 +0530

    complete auth

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,13 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
 
 export async function GET(request:Request, {params}:{params:any}) {
 	const endpoint = params.kindeAuth;
+	const result= await handleAuth(request, endpoint);
 
-	return await handleAuth(request, endpoint);
+	if(!result){
+		NextResponse.json("error",{status:402})
+
+	}
+	return result as Response
 }
